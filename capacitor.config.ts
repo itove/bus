3c36cf1e3116b0c387d5e0b98f7d34973e05cc8b
Al Zee <z@alz.ee>
2023-03-23 19:59:59 +0800

    nth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'bus',
+  appId: 'com.itove.bus',
+  appName: '智能公交',
   webDir: 'www',
   bundledWebRuntime: false
 };
