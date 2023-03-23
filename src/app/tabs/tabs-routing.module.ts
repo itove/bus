920b559479ebcb0ba83aacf7bc049dad866bd235
Al Zee <z@alz.ee>
2023-03-23 18:50:18 +0800

    tabs

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,31 +8,31 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'trip',
+        loadChildren: () => import('../trip/trip.module').then(m => m.TripPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'bus',
+        loadChildren: () => import('../bus/bus.module').then(m => m.BusPageModule)
       },
       {
         path: 'me',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../me/me.module').then(m => m.MePageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
